67eac17ff1897ec9b26a7df2b7fc1064e94a533c
Ryan Burnette <ryan.burnette@gmail.com>
2021-07-23 08:40:01 -0400

    compatibility
    - add compatibility with bundlers (module.exports)

--- ajquery.js
+++ ajquery.js
@@ -1,7 +1,17 @@
-function $(sel, el) {
-  return (el || document).querySelector(sel);
-}
+(function () {
+  function $(sel, el) {
+    return (el || document).querySelector(sel);
+  }
 
-function $$(sel, el) {
-  return (el || document).querySelectorAll(sel);
-}
+  function $$(sel, el) {
+    return (el || document).querySelectorAll(sel);
+  }
+
+  if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+    module.exports.$ = $;
+    module.exports.$$ = $$;
+  } else if (window) {
+    window.$ = $;
+    window.$$ = $$;
+  }
+})();
