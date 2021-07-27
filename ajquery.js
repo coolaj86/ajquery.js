a64fd3f93cb869d9b8939dc4101f8d62f4c028e9
AJ ONeal <coolaj86@gmail.com>
2021-07-27 17:08:55 -0600

    feature: $.on(sel, ev, fn) for persistent events

--- ajquery.js
+++ ajquery.js
@@ -5,3 +5,12 @@ function $(sel, el) {
 function $$(sel, el) {
   return (el || document).querySelectorAll(sel);
 }
+
+$.on = function (sel, event, fn) {
+  document.body.addEventListener(function (ev) {
+    if (!ev.target.matchSelector(sel)) {
+      return true;
+    }
+    fn.call(ev.target, ev);
+  });
+};
