2944bdbc4cdae399c70f5f0191edcc2d80a982dc
AJ ONeal <aj@therootcompany.com>
2024-08-29 16:23:40 -0600

    doc: add types to define args

--- ajquery.js
+++ ajquery.js
@@ -1,8 +1,18 @@
+/**
+ * Select first matching element, just like console $
+ * @param {String} cssSelector
+ * @param {ParentNode} [$parent=document]
+ */
 function $(cssSelector, $parent = document) {
   let $child = $parent.querySelector(cssSelector);
   return $child;
 }
 
+/**
+ * Select all matching child elements as a JS Array, just like console $$
+ * @param {String} cssSelector
+ * @param {ParentNode} [$parent=document]
+ */
 function $$(cssSelector, $parent = document) {
   let nodeList = $parent.querySelectorAll(cssSelector);
   let $children = Array.from(nodeList);
